8a77c0d201c981350ef3b5dfb1248119dcfbfa7f
Mahantesh888 <Administrator@172.31.11.137>
2024-04-25 07:17:55 +0000

    chromeoption

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -1,15 +1,18 @@
 package testScripts;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 public class GooglePageTest {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions option = new ChromeOptions();
+		option.setBrowserVersion("115");
+		WebDriver driver = new ChromeDriver(option);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.print("page title..."+ driver.getTitle());
-		driver.close();
+	    driver.close();
 		
 
 	}
